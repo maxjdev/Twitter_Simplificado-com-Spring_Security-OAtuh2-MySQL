ae0f6dfff74a5937663b9a2ab7857ae76a5ddcca
maxjdev <maxjramosdev@gmail.com>
2024-09-08 19:58:13 -0300

    feat: add main class

---
+++ src/main/java/br/com/twittersimplificado/TwittersimplificadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.twittersimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittersimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittersimplificadoApplication.class, args);
+	}
+
+}
