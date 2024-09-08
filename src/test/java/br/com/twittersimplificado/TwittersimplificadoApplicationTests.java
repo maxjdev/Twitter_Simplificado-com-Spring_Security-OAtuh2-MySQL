50f1eda8e2e86b4f9cb18321a24a4c954fef8a64
maxjdev <maxjramosdev@gmail.com>
2024-09-08 19:52:46 -0300

    initial settings

---
+++ src/test/java/br/com/twittersimplificado/TwittersimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.twittersimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwittersimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
